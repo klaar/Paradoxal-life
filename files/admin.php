1a6a39709f561224707e1598d51d9cd093fefcda
Yul Frankenberg <yul.saintmich@gmail.com>
2014-04-28 22:14:39 +0200

    Paradoxal Life

--- files/admin.php
+++ files/admin.php
@@ -1,8 +1,14 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Paradoxal life</title>	
 <link rel="stylesheet" href="../css/style.css">
 <link rel="stylesheet" href="../css/bootstrap.css">
 <link rel="stylesheet" href="../css/bootstrap.min.css">
 <link rel="stylesheet" href="../css/bootstrap-responsive.css">
+</head> 
+<?php include 'header.html'; ?>
+<body>
 
-<header>
-<center><h1 style="font-size: 8vmax">Paradoxal Life<h1></center>
-</header>
+</body>
+</html>
