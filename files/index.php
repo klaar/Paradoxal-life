ab5bc0b7c4d038002f55716900ccf6534cc8b680
Yul Frankenberg <yul.saintmich@gmail.com>
2014-04-28 21:11:31 +0200

    Paradoxal Life

---
+++ files/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/bootstrap.css">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/bootstrap-responsive.css">
+
+</head>
+<?php include 'header.html';?>
+<body>
+ 
+</body>
+</html>
